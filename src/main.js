6bfa69aa2d8440c9a81f91f571a18bcf1327e72b
watahari <4580778+watahari@users.noreply.github.com>
2020-08-29 23:05:12 +0900

    [add] Google Analytics tracking config

--- src/main.js
+++ src/main.js
@@ -2,8 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
+import VueGtag from "vue-gtag";
 
 Vue.config.productionTip = false
+
+Vue.use(VueGtag, {
+  config: {
+    id: "UA-176694381-1",
+    params: {
+      send_page_view: false
+    }
+  }
+});
 
 new Vue({
   vuetify,
