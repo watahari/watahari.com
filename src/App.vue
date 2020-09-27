5be06a7c7a5cfbf99f646c33b9af1809e1ac27d3
watahari <4580778+watahari@users.noreply.github.com>
2020-09-28 02:09:51 +0900

    [add] link to blog on header

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <a href="https://blog.watahari.com/">Blog</a>
     </div>
     <router-view/>
     <Footer/>
